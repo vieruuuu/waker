7224167c04f4a0fee5e296f268c8eedef9231a3e
Andrei Vieru <andrei.vieru@360design.ro>
2024-12-06 17:45:23 +0200

    Add firebase-tools

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,14 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [react(), tailwind({ applyBaseStyles: false }), db()],
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
